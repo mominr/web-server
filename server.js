f25908981f979aac22daaa27cff07eab6b8a3bc6
Rahim Momin <rahimmomin@gmail.com>
2015-11-26 22:43:14 -0600

    Update language

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var middleware = require('./middleware.js');
 app.use(middleware.logger);
 
 app.get('/About', middleware.requireAuthentication, function(req, res) {
-	res.send('About Us');	
+	res.send('About Us!');	
 });
 
 app.use(express.static(__dirname + '/public'));
